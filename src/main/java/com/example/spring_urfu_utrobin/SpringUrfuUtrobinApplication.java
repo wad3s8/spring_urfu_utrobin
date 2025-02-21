ce024c14b93a07821c91596bdcbea2fa35277a5f
Vlad Utrobin <vlad.utrobin.2006@mail.ru>
2025-02-21 20:22:18 +0500

    Initial commit

---
+++ src/main/java/com/example/spring_urfu_utrobin/SpringUrfuUtrobinApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_urfu_utrobin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUrfuUtrobinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUrfuUtrobinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_urfu_utrobin/SpringUrfuUtrobinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_urfu_utrobin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUrfuUtrobinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
